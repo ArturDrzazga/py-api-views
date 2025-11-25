c0a1c8c293a30b01334f9803f9db270ffa77cdca
Artur Drzazga <artur.drzazga03@gmail.com>
2025-11-25 16:02:25 +0100

    fixed solution

--- cinema/urls.py
+++ cinema/urls.py
@@ -11,21 +11,21 @@ from cinema.views import (GenreList,
 router = routers.DefaultRouter()
 router.register("movies", MovieViewSet)
 urlpatterns = [
-    path("genre/", GenreList.as_view(), name="genre-list"),
-    path("genre/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
-    path("actor/", ActorList.as_view(), name="actor-list"),
-    path("actor/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
-    path("cinemahall/", CinemaHallViewSet.as_view(actions={
+    path("genres/", GenreList.as_view(), name="genre-list"),
+    path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
+    path("actors/", ActorList.as_view(), name="actor-list"),
+    path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
+    path("cinema_halls/", CinemaHallViewSet.as_view(actions={
         "get": "list",
         "post": "create"
-    }), name="cinemahall-list"),
-    path("cinemahall/<int:pk>/", CinemaHallViewSet.as_view(actions={
+    }), name="cinemahalls-list"),
+    path("cinema_halls/<int:pk>/", CinemaHallViewSet.as_view(actions={
         "get": "retrieve",
         "put": "update",
         "patch": "partial_update",
         "delete": "destroy"
-    }), name="cinemahall-detail"),
-    path("movie/", include(router.urls)),
+    }), name="cinemahalls-detail"),
+    path("", include(router.urls)),
 ]
 
 app_name = "cinema"
